9acc70b6caf33a7ac79324043f600af595202b36
1071537 <50259231+1071537@users.noreply.github.com>
2019-06-16 01:50:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,127 @@
-$(document).ready(function(){
-    var currentQuiz = null;
-    $("#startButton").click(function()
-    {
-        if(currentQuiz==null)
-        {
-            currentQuiz=0;
-            $("#question").text(questions[0].question);
-            $("#options").empty();
-            for(var x = 0;x<questions[0].answers.length;x++)
-            {
-                $("#options").append("<input id='" + x + "' name='options' type='radio' value="+x+">"+"<label for='" + x + "'>"+questions[0].answers[x][0]+"</label><br></br>");
-            }
-            $("#startButton").attr("value","Next");
-        } 
-        else
-        {
-            $.each($(":radio"),function(i,val)
-            {
-                if(val.checked)
-                    {
-                        if(isNaN(questions[currentQuiz].answers[i][1]))
-                        {
-                            var finalResult = questions[currentQuiz].answers[i][1];
-                            $("#question").text(finalAnswers[finalResult][0]);
-                            $("#options").empty();
-                            $("#options").append(finalAnswers[finalResult][1]+"<br></br>");
-                            currentQuiz=null;
-                            $("#startButton").attr("value","重新開始");
-                        }
-                        else
-                        {
-                            currentQuiz=questions[currentQuiz].answers[i][1]-1;
-                            $("#question").text(questions[currentQuiz].question);
-                            $("#options").empty();
-                            for(var x = 0;x<questions[currentQuiz].answers.length;x++)
-                            {
-                                $("#options").append("<input id='" + x + "' name='options' type='radio' value="+x+">"+"<label for='" + x + "'>"+questions[0].answers[x][0]+"</label><br></br>");
-                            }
-                        }
-                        return false;
-                    }
-                            
-                });
-       }
-                           
-    });
-                           });
+/*
+	Road Trip by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		var $height = $('#header').height();
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Banner
+
+			if ($banner.length > 0) {
+
+				// IE: Height fix.
+					if (skel.vars.browser == 'ie'
+					&&	skel.vars.IEVersion > 9) {
+
+						skel.on('-small !small', function() {
+							$banner.css('height', '100vh');
+						});
+
+						skel.on('+small', function() {
+							$banner.css('height', '');
+						});
+
+					}
+
+				// More button.
+					$banner.find('.more')
+						.addClass('scrolly');
+
+			}
+
+
+		// Get BG Image
+
+			if ( $( ".bg-img" ).length ) {
+
+				$( ".bg-img" ).each(function() {
+
+					var post 	= $(this),
+						bg 		= post.data('bg');
+
+					post.css( 'background-image', 'url(images/' + bg + ')' );
+
+				});
+
+
+			}
+
+		// Posts
+
+			$( ".post" ).each( function() {
+				var p = $(this),
+					i = p.find('.inner'),
+					m = p.find('.more');
+
+				m.addClass('scrolly');
+
+				p.scrollex({
+					top: '40vh',
+					bottom: '40vh',
+					terminate: 	function() { m.removeClass('current'); i.removeClass('current'); },
+					enter: 		function() { m.addClass('current'); i.addClass('current'); },
+					leave: 		function() { m.removeClass('current'); i.removeClass('current'); }
+				});
+
+			});
+
+		// Scrolly.
+			if ( $( ".scrolly" ).length ) {
+
+				$('.scrolly').scrolly();
+			}
+
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
+
+	});
+
+})(jQuery);
